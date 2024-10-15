faaaa2267fe0c6a56bc676fb1200932924cf80cc
MIKEL FAJARDO BONASTRE <mfajardo001@ikasle.ehu.eus>
2024-10-16 00:37:12 +0200

    otra prueva maven

--- Rides24/src/main/java/gui/AdminGUI.java
+++ Rides24/src/main/java/gui/AdminGUI.java
@@ -76,7 +76,7 @@ public class AdminGUI extends JFrame {
 		jButtonItxi = new JButton();
 		jButtonItxi.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				jButtonClose_actionPerformed(e);
+				jbuttonclose_actionperformed(e);
 			}
 		});
 		jButtonItxi.setText(ResourceBundle.getBundle("Etiquetas").getString("EgoeraGUI.Close"));
@@ -93,7 +93,7 @@ public class AdminGUI extends JFrame {
 
 	}
 
-	private void jButtonClose_actionPerformed(ActionEvent e) {
+	private void jbuttonclose_actionperformed(ActionEvent e) {
 		this.setVisible(false);
 	}
 
